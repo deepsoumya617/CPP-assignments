fa27336f3610877b72ef426e9d33e320e0f293ce
deepsoumya617 <soumyadeepghosh617@gmail.com>
2023-11-01 00:20:08 +0530

    ``Question 01 solution``

--- 1D-array/array1/code/main.cpp
+++ 1D-array/array1/code/main.cpp
@@ -1,6 +1,29 @@
+// Calculate the product of all the elements in the given array.
 #include <bits/stdc++.h>
 using namespace std;
- 
-int main(){
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array - ";
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter array elements - ";
+    // Taking Inputs
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Finding product
+    int product = 1;
+    for (int i = 0; i < n; i++)
+    {
+        product *= arr[i];
+    }
+
+    // Printing the result
+    cout << "Product = " << product;
     return 0;
 }

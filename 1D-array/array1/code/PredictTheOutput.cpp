1ee1be090069c136dd459d23b1a42bde2554da8f
deepsoumya617 <soumyadeepghosh617@gmail.com>
2023-11-01 10:29:29 +0530

    ```Output of Question 06``

---
+++ 1D-array/array1/code/PredictTheOutput.cpp
@@ -0,0 +1,16 @@
+// Predict the output.
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int sub[50], i;
+    for (i = 0; i <= 48; i++);
+    {
+        sub[i] = i;
+        cout << sub[i] << endl;
+    }
+    return 0;
+}
+
+// The Output will be 49
